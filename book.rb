1d7364cf845b0dc8013bd8bc50861eb631ca8937
Sylvestre <pasytchangwa@gmail.com>
2021-10-15 11:05:44 +0300

    Modify book.rb file

--- book.rb
+++ book.rb
@@ -1,10 +1,16 @@
 class Book
   attr_accessor :title, :author, :rentals
+  attr_reader :id
 
   def initialize(title, author)
     @title = title
     @author = author
     @rentals = []
+  end
+  
+  def add_rental(rental)
+    @rentals.push(rental)
+    rental.book = self
   end
 
   def to_s
